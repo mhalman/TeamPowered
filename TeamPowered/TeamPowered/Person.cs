e5bc54dfcd08fb7a8a2e0bdab52ffa0d75085ed8
Mark Halman <markhalman@gmail.com>
2017-06-13 14:54:57 +0100

    Add new properties for the person class

--- TeamPowered/TeamPowered/Person.cs
+++ TeamPowered/TeamPowered/Person.cs
@@ -8,7 +8,7 @@ namespace TeamPowered
 {
     class Person
     {
-            public string Name { get; set }
-            Public string Address { get; set }
+        public string Name { get; set; }
+        public string Address { get; set; }
     }
 }
